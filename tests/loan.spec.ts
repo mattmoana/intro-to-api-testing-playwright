09a656581d865b5caf3fbc2bb32ed9d44acf8499
mattmoana <matt.morozov@oc.eu>
2024-10-24 10:47:22 +0300

    added loan tests that check for status codes and response body

---
+++ tests/loan.spec.ts
@@ -0,0 +1,61 @@
+import { expect, test } from '@playwright/test'
+import { LoanDto } from './dto/loan-dto'
+import { StatusCodes } from 'http-status-codes'
+
+test.describe('Loan Decision API Tests', () => {
+
+  test('positive case should return 200 with medium risk', async ({ request }) => {
+    const riskDto = LoanDto.calcPositiveRiskScore()
+    riskDto.loanPeriod = 9
+    const response = await request.post(
+      'https://backend.tallinn-learning.ee/api/loan-calc/decision',
+      {
+        data: riskDto,
+      },
+    )
+    expect.soft(response.status()).toBe(StatusCodes.OK)
+    const responseBody = await response.json()
+    expect.soft(responseBody.riskLevel).toBe('Medium Risk')
+  })
+
+  test('positive case should return 200 with high risk', async ({ request, }) => {
+    const riskDto = LoanDto.calcPositiveRiskScore()
+    riskDto.loanPeriod = 3
+    const response = await request.post(
+      'https://backend.tallinn-learning.ee/api/loan-calc/decision',
+      {
+        data: riskDto,
+      },
+    )
+    const responseBody = await response.json()
+    expect.soft(response.status()).toBe(StatusCodes.OK)
+    expect.soft(responseBody.riskLevel).toBe('High Risk')
+
+  })
+  test('positive case returns 200 but decision is negative', async ({ request, }) => {
+    const riskDto = LoanDto.calcPositiveRiskScore()
+    riskDto.income = 1000
+    riskDto.loanPeriod = 36
+    const response = await request.post(
+      'https://backend.tallinn-learning.ee/api/loan-calc/decision',
+      {
+        data: riskDto,
+      },
+    )
+    const responseBody = await response.json()
+    expect.soft(response.status()).toBe(StatusCodes.OK)
+    expect.soft(responseBody.riskDecision).toBe('negative')
+  })
+
+  test('negative case returns 400 where debt is negative', async ({ request, }) => {
+    const riskDto = LoanDto.calcNegativeRiskScore()
+    riskDto.debt = -12
+    const response = await request.post(
+      'https://backend.tallinn-learning.ee/api/loan-calc/decision',
+      {
+        data: riskDto,
+      },
+    )
+    expect.soft(response.status()).toBe(StatusCodes.BAD_REQUEST)
+  })
+})
--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test'
 
 import { StatusCodes } from 'http-status-codes'
+import { OrderDto } from './dto/order-dto'
 
 test('get order with correct id should receive code 200', async ({ request }) => {
   // Build and send a GET request to the server
@@ -23,39 +24,28 @@ test('get order with correct id should receive code 400', async ({ request }) =>
 })
 
 test('post order with correct data should receive code 201', async ({ request }) => {
+  const orderDto = OrderDto.createOrderWithCorrectRandomData()
+  orderDto.customerName = "Sam"
   // prepare request body
-  const requestBody = {
-    status: 'OPEN',
-    courierId: 0,
-    customerName: 'string',
-    customerPhone: 'string',
-    comment: 'string',
-    id: 0,
-  }
+  // const orderDto = new OrderDto("OPEN", 0, "Matt", "Morozov", "testing", 0)
   // Send a POST request to the server
   const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
-    data: requestBody,
+    data: orderDto
   })
   // Log the response status and body
   console.log('response status:', response.status())
   console.log('response body:', await response.json())
-  expect(response.status()).toBe(StatusCodes.OK)
+  expect.soft(response.status()).toBe(StatusCodes.OK)
+
+  const responseBody = await response.json()
+  expect.soft(response.status()).toBe(StatusCodes.OK)
+  expect.soft(responseBody.status).toBe("OEN")
+  expect.soft(responseBody.customerName).toBe("Sam")
 })
 
 test('post order with incorrect data should receive code 400', async ({ request }) => {
-  // prepare request body
-  const requestBody = {
-    status: '444',
-    courierId: 0,
-    customerName: 'string',
-    customerPhone: 'string',
-    comment: 'string',
-    id: 0,
-  }
-  // Send a POST request to the server
   const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
-    data: requestBody,
+    data: OrderDto.createOrderWithInCorrectRandomData()
   })
-  // Log the response status and body
-  expect(response.status()).toBe(400)
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
