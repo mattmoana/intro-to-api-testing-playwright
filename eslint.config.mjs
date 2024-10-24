ae7efdc9c5498d4c8fb4cee40219f3bb0ab75163
mattmoana <matt.morozov@oc.eu>
2024-10-24 20:50:58 +0300

    commented out eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,7 +20,7 @@ export default [
         'error',
         { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
       ],
-      '@typescript-eslint/no-unused-expressions': 'error',
+      // '@typescript-eslint/no-unused-expressions': 'error',
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/explicit-function-return-type': 'warn',
       'no-undef': 'error',
